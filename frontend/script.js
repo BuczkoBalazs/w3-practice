eb5ba38e3f47670cbc520f1809d640f67085b31d
BuczkoBalazs <buci85@gmail.com>
2022-02-03 16:04:48 +0100

    asd

--- frontend/script.js
+++ frontend/script.js
@@ -1,14 +1,15 @@
-function addTwoIntegers(firstInteger, secondInteger) {
-    // console.log(firstInteger + secondInteger)
-    // return firstInteger + secondInteger
-    // return "Hello dicsak"
-    if(typeof firstInteger === "number" && typeof secondInteger === "number"){
-        return firstInteger + secondInteger
-    } else {
-        return "Valamelyik argumentum nem szám. Ezért a művelet nem elvégezhető."
-    }
-}
+// function addTwoIntegers(firstInteger, secondInteger) {
+//     // console.log(firstInteger + secondInteger)
+//     // return firstInteger + secondInteger
+//     // return "Hello dicsak"
+//     if(typeof firstInteger === "number" && typeof secondInteger === "number"){
+//         return firstInteger + secondInteger
+//     } else {
+//         return "Valamelyik argumentum nem szám. Ezért a művelet nem elvégezhető."
+//     }
+// }
 
-console.log( addTwoIntegers(12, "13") );
-console.log( addTwoIntegers(1920, 85) );
-console.log(0 ==="0")
+// console.log( addTwoIntegers(12, "13") );
+// console.log( addTwoIntegers(1920, 85) );
+// console.log(0 ==="0")
+
